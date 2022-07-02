adab6929f39f59a7af57bdb190679423ae2a33b5
Serjo750 <you@example.com>
2022-07-02 14:49:17 +0300

    validation the path

--- pipex/src/Check_fork.c
+++ pipex/src/Check_fork.c
@@ -6,8 +6,26 @@ int main(int argc, char *argv[], char *envp[])
 {
     (void)argc;
     (void)argv;
-    (void)envp;
-    ft_printf("%d\n", access("/usr/local/bin/", F_OK));
-    
+    int i = 0;
+    int j = 0;
+    int z = 0;
+    char *path;
+    while(envp[i])
+    {
+        if (ft_strncmp(envp[i], "PATH", ft_strlen("PATH")) == 0)
+        {
+            while(envp[i][j] != '=' && envp[i][j])
+                j++;
+            z = ++j;
+            while(envp[i][z] != ':' && envp[i][z])
+                z++;
+            path = ft_substr(envp[i], j, z - j);
+            path = ft_strjoin(path, "/");
+            ft_printf("Path is %d!::%s\n",access(path, F_OK),path);
+
+            
+        }
+        i++;
+    }
     return 0;
 }
